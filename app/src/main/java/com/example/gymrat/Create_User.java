81b694726e535d7a8efe7200d57c6bd57149a3ff
Jonne <clownfiesta98@gmail.com>
2022-04-22 22:30:39 +0300

    Starting pages vol 3.1

--- app/src/main/java/com/example/gymrat/Create_User.java
+++ app/src/main/java/com/example/gymrat/Create_User.java
@@ -1,36 +1,27 @@
 package com.example.gymrat;
 
-import android.content.Context;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.preference.TwoStatePreference;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
 
 public class Create_User extends AppCompatActivity {
 
-    private static RadioButton male;
     private EditText nameText;
 
-    private RadioGroup radioSexGroup;
-    private static RadioButton female;
-    private static SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_time_layout);
-        Intent intent = getIntent();
     }
 
     public void startButton(View v){
@@ -41,8 +32,8 @@ public class Create_User extends AppCompatActivity {
         nameText = (EditText) findViewById(R.id.kayttajatunnus);
         String name = nameText.getText().toString();
 
-        male = (RadioButton)findViewById(R.id.man);
-        female = (RadioButton)findViewById(R.id.woman);
+        RadioButton male = (RadioButton) findViewById(R.id.man);
+        RadioButton female = (RadioButton) findViewById(R.id.woman);
 
 
         if(name.matches("" ) | !male.isChecked() & !female.isChecked()) {
@@ -57,13 +48,10 @@ public class Create_User extends AppCompatActivity {
     private void saveGenderInPreference() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        radioSexGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        RadioGroup radioSexGroup = (RadioGroup) findViewById(R.id.radioGroup);
         int selectedId = radioSexGroup.getCheckedRadioButtonId();
-        if(selectedId == R.id.man)
-            editor.putBoolean("is_male", true);
-        else
-            editor.putBoolean("is_male", false);
-        editor.commit();
+        editor.putBoolean("is_male", selectedId == R.id.man);
+        editor.apply();
     }
 
     public void saveUserName(){
