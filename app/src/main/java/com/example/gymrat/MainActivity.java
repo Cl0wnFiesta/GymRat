d8b81b4849a075ab8eacbfa2f1d36f18f435991a
Jonne <clownfiesta98@gmail.com>
2022-04-22 16:07:58 +0300

    Starting pages

--- app/src/main/java/com/example/gymrat/MainActivity.java
+++ app/src/main/java/com/example/gymrat/MainActivity.java
@@ -3,21 +3,24 @@ package com.example.gymrat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean firstTime = true;
+
+    private SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is a comment
-        if(firstTime){
-            Log.d("tag","why?");
-            Intent intent = new Intent(this, FirstTime.class);
+        prefs = getSharedPreferences("hasRunBefore", 0);
+        Boolean firstTime = prefs.getBoolean("hasRun", false);
+
+        if(!firstTime){
+            Intent intent = new Intent(this, Create_User.class);
             startActivity(intent);
         }
     }
