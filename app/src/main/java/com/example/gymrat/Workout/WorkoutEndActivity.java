48e761544430ce41eb59d6b111044a26a90e7ba1
Henriui <henri.uimonen@gmail.com>
2022-04-28 17:35:48 +0300

    WorkoutEndiin UI elementtejä ja preferenssien haku

--- app/src/main/java/com/example/gymrat/Workout/WorkoutEndActivity.java
+++ app/src/main/java/com/example/gymrat/Workout/WorkoutEndActivity.java
@@ -2,15 +2,30 @@ package com.example.gymrat.Workout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.gymrat.R;
 
 public class WorkoutEndActivity extends AppCompatActivity {
+    SharedPreferences sp;
+    private double maxPenkki, maxKyykky, maxMaastaveto, maxPystypunnerrus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_end);
+        fetchPreferences(); //haetaan käyttäjän preferenssit suorituksen tietokantaan tallennusta varten
     }
+
+    //Hakee käyttäjän preferenssit
+    private void fetchPreferences(){
+        sp = getSharedPreferences("myKey", MODE_PRIVATE);
+        maxPenkki = Double.parseDouble(sp.getString("penkki", "0"));
+        maxKyykky = Double.parseDouble(sp.getString("kyykky", "0"));
+        maxMaastaveto = Double.parseDouble(sp.getString("maastaveto", "0"));
+        maxPystypunnerrus = Double.parseDouble(sp.getString("pystypunnerrus", "0"));
+    }
+
+
 }
